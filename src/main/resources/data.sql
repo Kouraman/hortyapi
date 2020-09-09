35521d631c327fcd6f71ed27507c0dcffd3cbf9f
Kourama <nicolas.brieussel@gmail.com>
2020-09-10 01:14:09 +0200

    add some data in database, and change properties to stop reinitialize the database at each startup

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -19,7 +19,7 @@ SET @batabl = 'https://jardinage.lemonde.fr/images/dossiers/2017-07/batavia-1534
 SET @batabr = 'https://locavor.fr/data/produits/4/81168/81168-batavia-brune-1.jpg';
 SET @choufri = 'https://cdn.panierlocal.org/media/PXJTJUXEC/chou-vert_zoom.jpg';
 SET @choufleu = 'https://www.carrefour.fr/media/1500x1500/Photosite/PRODUITS_FRAIS_TRANSFORMATION/FRUITS_ET_LEGUMES/3000001038436_PHOTOSITE_20150617_162744_0.jpg?placeholder=1';
-SET @mache = 'https://www.monpetitcoinvert.com/blog/wp-content/uploads/2016/10/mache.jpg'
+SET @mache = 'https://www.monpetitcoinvert.com/blog/wp-content/uploads/2016/10/mache.jpg';
 
 INSERT INTO categories (name)
  VALUES
@@ -30,7 +30,7 @@ INSERT INTO categories (name)
 INSERT INTO families (category_id,name,src_img)
  VALUES
  ((SELECT id from categories WHERE name LIKE 'LEGUMES'),'Choux',@choufri),
- ((SELECT id from categories WHERE name LIKE 'LEGUMES'),'Salade',@batabl)
+ ((SELECT id from categories WHERE name LIKE 'LEGUMES'),'Salades',@batabl)
 ;
 
 INSERT INTO regroupments (family_id,name,start_plating,end_plating)
@@ -38,10 +38,10 @@ INSERT INTO regroupments (family_id,name,start_plating,end_plating)
  ((SELECT id from families WHERE name LIKE 'Choux'),'Choux d''automne','2019-07-01','2019-07-31'),
  ((SELECT id from families WHERE name LIKE 'Choux'),'Choux d''hiver','2019-07-21','2019-08-31'),
  ((SELECT id from families WHERE name LIKE 'Choux'),'Choux de printemps','2019-09-21','2019-10-21'),
- ((SELECT id from families WHERE name LIKE 'Salade'),'Salade d''hiver','2019-10-01','2019-01-31'),
- ((SELECT id from families WHERE name LIKE 'Salade'),'Salade de printemps','2019-02-01','2019-05-15'),
- ((SELECT id from families WHERE name LIKE 'Salade'),'Salade d''été','2019-05-15','2019-08-15'),
- ((SELECT id from families WHERE name LIKE 'Salade'),'Salade d''automne','2019-08-01','2019-10-15')
+ ((SELECT id from families WHERE name LIKE 'Salades'),'Salade d''hiver','2019-10-01','2019-01-31'),
+ ((SELECT id from families WHERE name LIKE 'Salades'),'Salade de printemps','2019-02-01','2019-05-15'),
+ ((SELECT id from families WHERE name LIKE 'Salades'),'Salade d''été','2019-05-15','2019-08-15'),
+ ((SELECT id from families WHERE name LIKE 'Salades'),'Salade d''automne','2019-08-01','2019-10-15')
 ;
 
 INSERT INTO plants (group_id,name,src_img)
@@ -63,21 +63,30 @@ INSERT INTO plants (group_id,name,src_img)
  ((SELECT id from regroupments WHERE name LIKE 'Salade d''automne'),'Batavia Brune',@batabr)
 ;
 
-INSERT INTO generic_prices (family_id,price,quantité)
+INSERT INTO generic_prices (family_id,price,quantity)
   VALUES
   ((SELECT id from families WHERE name LIKE 'Choux'),0.60,1),
   ((SELECT id from families WHERE name LIKE 'Choux'),5.50,10),
   ((SELECT id from families WHERE name LIKE 'Salades'),2.40,12)
 ;
 
-INSERT INTO generic_prices (family_id,price,quantité)
+INSERT INTO generic_advices (family_id,advice)
   VALUES
-  ((SELECT id from families WHERE name LIKE 'Choux'),0.60,1),
-  ((SELECT id from families WHERE name LIKE 'Choux'),5.50,10),
-  ((SELECT id from families WHERE name LIKE 'Salades'),2.40,12)
+  ((SELECT id from families WHERE name LIKE 'Choux'),'Les pieds en l''air'),
+  ((SELECT id from families WHERE name LIKE 'Choux'),'La tête en bas')
 ;
 
-INSERT INTO specific_prices (family_id,price,quantité)
+INSERT INTO generic_treatments (family_id,treatment)
+  VALUES
+  ((SELECT id from families WHERE name LIKE 'Choux'),'D6')
+;
+
+INSERT INTO specific_prices (plant_id,price,quantity)
   VALUES
   ((SELECT id from plants WHERE name LIKE 'Mache'),2,12)
+;
+
+INSERT INTO specific_advices (plant_id,advice)
+  VALUES
+  ((SELECT id from plants WHERE name LIKE 'Mache'),'Mangez la en vinaigrette')
 ;

db93c8255c97bccfbfef963a4e7f7bd7f380ba92
Kouraman <nicolas.brieussel@gmail.com>
2020-10-18 15:20:49 +0200

    edit csv_editor.py to make not erase .csv file, and remove generated files from git

--- exceldata/csv_editor.py
+++ exceldata/csv_editor.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python
 
 import sys
+import os
 
 #   run with :: python csv_editor.py data.csv
 #   will edit file to Mysql INSERT format
 #   ie. ::     data1;data2;.....;dataN   >>   (data1,data2,....,dataN)
 
 
-def main(fileName):
+def editCSVFile(fileName):
 
     file = open(fileName, 'r+')
     lines = file.readlines()
@@ -25,9 +26,22 @@ def main(fileName):
 
         lines[index]=lines[index]+"\n"              #read the line return at each line
 
-    file = open(fileName, "w")
+    file = open("csv_edited_"+fileName, "w+")
     file.write("".join(lines))
     file.close()
 
+def main():
+
+    fileNames = os.listdir()
+    csvFileNames = list()
+
+    for fileName in fileNames:
+        if fileName.endswith(".csv") and not(fileName.startswith("csv_edited")):
+            csvFileNames.append(fileName)
+
+    for fileName in csvFileNames:
+        editCSVFile(fileName)
+
+
 if (__name__ == "__main__"):
-    main(sys.argv[1])
+    main()
--- exceldata/editcsv.bat
+++
@@ -1,8 +0,0 @@
-python csv_editor.py categories.csv
-python csv_editor.py planting_dates.csv
-python csv_editor.py plants.csv
-python csv_editor.py plants_images.csv
-python csv_editor.py plants_plating_dates.csv
-python csv_editor.py plants_prices.csv
-python csv_editor.py prices.csv
-python csv_editor.py regroupments.csv
